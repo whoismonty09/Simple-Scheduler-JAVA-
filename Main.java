b5b0fcc3ec14fc34c840788d7d740f8f22e5e773
Mayank malviya <mayankmalviya440@gmail.com>
2026-02-15 10:23:37 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
+import java.util.Timer;
+import java.util.TimerTask;
 
+class Task extends TimerTask {
+    public void run() {
+        System.out.println("Task executed at: " + System.currentTimeMillis());
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new Task(), 0, 2000);
+    }
+}
